89fda0d7d4ef09193541fac5a471c7de5ad52789
Fiza-Fakher <fizafakher62@gmail.com>
2025-11-12 17:46:35 +0500

    :sparkles:fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -36,7 +36,7 @@ function Footer() {
             {/* --- Column 1: Logo & Info --- */}
             <div>
               <div className="flex items-center gap-3 mb-4">
-                <img src="/logo.png" alt="logo" className="" />
+                <img src="/logo.png" alt="logo" className="w-20" />
                 
               </div>
               <p className="text-gray-300 leading-relaxed text-[15px]">
